0af1077fb655220e3d191da7c6c1c1ab0015a7a7
Mafka <kitm@digt.ru>
2023-05-28 09:29:58 +0300

    untitled3

---
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -0,0 +1,17 @@
+package ru.netology.javaqa;
+
+import ru.netology.javaqa.Post;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+    }
+
+
+}
